a42198ad99afa8db5d2eccf53d4b149849d4c0c6
ashokkumar2342 <ashokkumar2342@gmail.com>
2018-12-20 22:16:55 +0530

    sms change

--- app/Listeners/SendSms.php
+++ app/Listeners/SendSms.php
@@ -48,14 +48,25 @@ class SendSms
         
         
         
-         $msg=urlencode($event->message);
+        // $msg=urlencode($event->message);
  
-        $url = "http://bulksms.innovusine.com/sendurlcomma.aspx?user=20084062&pwd=zad@14&senderid=ZADRTK&mobileno=$event->mobile&msgtext=$msg";
+        // $url = "http://bulksms.innovusine.com/sendurlcomma.aspx?user=20089373&pwd=123456&senderid=ZADRTK&mobileno=$event->mobile&msgtext=$msg&smstype=13";
+        // $ch = curl_init($url);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // $curl_scraped_page = curl_exec($ch);
+        // curl_close($ch);
+      
+        // Log::info($event->mobile.' : '.$event->message);
+
+        $msg=urlencode($event->message);
+        
+        
+        $url = "http://203.129.225.68/API/WebSMS/Http/v1.0a/index.php?username=zgsrtk1&password=123456&sender=zgsrtk&to=$event->mobile&message=$msg&reqid=1&format=json&route_id=428&sendondate=date()";
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $curl_scraped_page = curl_exec($ch);
         curl_close($ch);
-      
+        
         Log::info($event->mobile.' : '.$event->message);
          
     }
