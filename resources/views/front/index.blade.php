1fa8e68f3b587456f8aeb5e9be4cf33d79102861
ashokkumar2342 <ashokkumar2342@gmail.com>
2018-12-29 16:39:57 +0530

    Update index.blade.php

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -506,8 +506,7 @@
 @if(Session::has('message'))
 <script type="text/javascript">
     Command: toastr["{{ Session::get('class') }}"]("{{ Session::get('message') }}");
-</script>
-
+</script> 
 @endif
 <script type="text/javascript">  
      $(window).load(function(){        
