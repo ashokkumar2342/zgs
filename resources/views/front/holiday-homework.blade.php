d338f89057e973ece02b33da0061d26e7f34ea07
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-05-23 20:25:04 +0530

    update homework

--- resources/views/front/holiday-homework.blade.php
+++ resources/views/front/holiday-homework.blade.php
@@ -19,7 +19,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-9 col-sm-9 col-xs-12">
-             <h4> SUMMER VACATION HOLIDAY HOMEWORK 2018-2019</h4>  
+            <h4> SUMMER VACATION HOLIDAY HOMEWORK 2019-2020</h4>   
              <table class="table"> 
                
              <thead>
